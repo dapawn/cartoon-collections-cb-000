ce68b2b3bd73e4291c1ae0f02bf54e05c3d4b022
David Kuruvilla <dapawn@gmail.com>
2017-10-19 14:30:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(arr)
 end
 
 def summon_captain_planet(arr)
-  arr.map { |e| e.map(&:capitalize) << "!" }
+  arr.map { |e| e.capitalize) << "!" }
 end
 
 def long_planeteer_calls(arr)
