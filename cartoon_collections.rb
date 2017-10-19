3baa7405feec6314961bd170581d10a198958450
David Kuruvilla <dapawn@gmail.com>
2017-10-19 14:20:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,15 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(arr)
+  arr.each {|n| puts n}
+end
+
+def summon_captain_planet(arr)
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def long_planeteer_calls(arr)
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
